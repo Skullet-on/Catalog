f79c26baa5663915f10b1e8d8e6794e09caec810
Skullet-on <skullet-on@mail.ru>
2016-10-24 17:28:14 +0300

    Add devise authentication

---
+++ spec/features/signin_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper.rb'
+
+feature "signing in" do
+	let(:user) { create(:user) }
+
+	def fill_in_signin_fields
+		fill_in "user[email]", with: user.email
+		fill_in "user[password]", with: user.password
+		click_button "Log in"
+	end
+
+	scenario "Visiting the site to sign in" do
+		visit root_path
+		click_link "Sign in"
+		fill_in_signin_fields
+		expect(page).to have_content("Signed in successfully.")
+	end
+end
